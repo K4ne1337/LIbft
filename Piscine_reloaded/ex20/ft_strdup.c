c91960c15fba9db312a50480a7d101d6d590c047
Arthur BELKHAYAT <abelkhay@e2r13p14.42.fr>
2018-11-12 16:26:06 +0100

    git perso

---
+++ Piscine_reloaded/ex20/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelkhay <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/08 13:23:42 by abelkhay          #+#    #+#             */
+/*   Updated: 2018/11/09 13:13:32 by abelkhay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		len;
+	char	*dest;
+
+	i = 0;
+	len = 0;
+	while (src[len])
+	{
+		len++;
+	}
+	dest = (char*)malloc(sizeof(char) * len + 1);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ Piscine_reloaded/ex27/main.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelkhay <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/09 11:54:27 by abelkhay          #+#    #+#             */
+/*   Updated: 2018/11/09 14:15:36 by abelkhay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <unistd.h>
+#define BUF_SIZE 10
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	int		ret;
+	char	buf[BUF_SIZE + 1];
+
+	fd = open(argv[1], O_RDONLY);
+	if (argc > 2)
+	{
+		write(2, "Too many arguments.\n", 20);
+		return (1);
+	}
+	if (fd == -1)
+	{
+		write(2, "File name missing.\n", 19);
+		return (1);
+	}
+	while ((ret = read(fd, buf, BUF_SIZE)))
+	{
+		buf[ret] = '\0';
+		ft_putstr(buf);
+	}
+	if (close(fd) == -1)
+		return (1);
+	return (0);
+}
